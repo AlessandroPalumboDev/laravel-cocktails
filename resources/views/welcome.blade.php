70f3649dd0778281e63ca63886eeb928c8524c18
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-11 16:38:31 +0200

    [UPD] Cambio rappresentazione homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,9 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <img src="{{ Vite::asset('resources/img/pipistrello.jpg')}}" alt="">
-                <ul>
-                    <li><a href="">prova list item e link</a></li>
-                </ul>
+                <h1 class="text-center py-5 my-5">
+                    Home Page
+                </h1>
             </div>
         </div>
     </div>
