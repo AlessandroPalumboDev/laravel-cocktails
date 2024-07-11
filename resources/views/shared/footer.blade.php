7afab0e3d8008417740ff7bfad6d1567713d28d8
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-11 17:26:34 +0200

    [UPD] cambio header e footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,8 +1,27 @@
 <footer>
 
-    <span class="rock-salt-regular text-body-tertiary p-1 m-1">
-        <i class="fas fa-yin-yang"></i> AP.
-    </span>
+<ul>
+    <li>
+        <h3 class="text-secondary">
+            Team
+        </h3>
+    </li>
+    <li>
+        Alessandro Palumbo
+    </li>
+    <li>
+        Salvatore Briga
+    </li>
+    <li>
+        Michele Raisa
+    </li>
+    <li>
+        Michele Aliffi
+    </li>
+    <li>
+        Samuele Paratore
+    </li>
+</ul>
 
 
 </footer>
