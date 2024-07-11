023f0d7cb9ff8da1bcd2fcba9df17a0d9c381fad
salvatorebriga <salvo.z2302@gmail.com>
2024-07-11 17:56:41 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index']);
+Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/cocktail', [CocktailController::class, 'cocktail'])->name('cocktail');
