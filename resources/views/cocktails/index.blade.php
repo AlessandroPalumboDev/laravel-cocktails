21305bed1873dbf52ad7deaf5d4c7195b8db0539
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-22 17:17:12 +0200

    [FIX] Bottoni su index

--- resources/views/cocktails/index.blade.php
+++ resources/views/cocktails/index.blade.php
@@ -14,12 +14,22 @@
                             <p><strong>Method:</strong> {{ $cocktail->method }}</p>
                             <p><strong>Glass Type:</strong> {{ $cocktail->glass_type }}</p>
                             <p><strong>Price:</strong> {{ $cocktail->price }}$</p>
+                            <a
+                                href="{{ route('cocktails.show', $cocktail) }}"
+                                class="btn btn-outline-primary btn-sm">
+                                <i class="fas fa-binoculars"></i>
+                            </a>
+                            <a href="{{ route('cocktails.edit', $cocktail) }}" class="btn btn-outline-warning btn-sm">
+                                <i class="far fa-pen-to-square"></i>
+                            </a>
+
                             <form action="{{ route('cocktails.destroy', $cocktail->id) }}" method="POST"
                                 style="display:inline;" onsubmit="return confirmDelete()">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger"
-                                    onclick="return confirm('Are you sure you want to delete this cocktail?')">Delete</button>
+                                <button type="submit" class="btn btn-outline-danger btn-sm"
+                                    onclick="return confirm('Are you sure you want to delete this cocktail?')">
+                                    <i class="far fa-trash-can"></i></button>
                             </form>
                         </div>
                     </div>
