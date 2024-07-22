f0fa57bc92fd3ec3aed18c9839ab1ad526cda6a5
salvatorebriga <salvo.z2302@gmail.com>
2024-07-22 16:53:05 +0200

    fix errors and data preserving

--- resources/views/cocktails/create.blade.php
+++ resources/views/cocktails/create.blade.php
@@ -3,35 +3,48 @@
 @section('main')
   <div class="container mt-5">
     <h1 class="mb-3">Create New Cocktail</h1>
+
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
     <form action="{{ route('cocktails.store') }}" method="POST" enctype="multipart/form-data">
       @csrf
       <div class="form-group mb-3">
         <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" name="name" required>
+        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
       </div>
       <div class="form-group mb-3">
         <label for="description">Description</label>
-        <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+        <textarea class="form-control" id="description" name="description" rows="3" required>{{ old('description') }}</textarea>
       </div>
       <div class="form-group mb-3">
         <label for="ingredients">Ingredients</label>
-        <textarea class="form-control" id="ingredients" name="ingredients" rows="3" required></textarea>
+        <textarea class="form-control" id="ingredients" name="ingredients" rows="3" required>{{ old('ingredients') }}</textarea>
       </div>
       <div class="form-group mb-3">
         <label for="method">Method</label>
-        <textarea class="form-control" id="method" name="method" rows="3" required></textarea>
+        <textarea class="form-control" id="method" name="method" rows="3" required>{{ old('method') }}</textarea>
       </div>
       <div class="form-group mb-3">
         <label for="glass_type">Glass Type</label>
-        <input type="text" class="form-control" id="glass_type" name="glass_type" required>
+        <input type="text" class="form-control" id="glass_type" name="glass_type" value="{{ old('glass_type') }}"
+          required>
       </div>
       <div class="form-group mb-3">
         <label for="price">Price ($)</label>
-        <input type="number" step="0.01" class="form-control" id="price" name="price" required>
+        <input type="number" step="0.01" class="form-control" id="price" name="price"
+          value="{{ old('price') }}" required>
       </div>
       <div class="form-group mb-3">
         <label for="img">Image</label>
-        <input type="text" class="form-control" id="img" name="img" required>
+        <input type="text" class="form-control" id="img" name="img" value="{{ old('img') }}" required>
       </div>
       <button type="submit" class="btn btn-primary mb-5">Create Cocktail</button>
     </form>
