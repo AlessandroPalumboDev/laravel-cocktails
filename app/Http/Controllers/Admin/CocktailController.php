2b27fb4b342b0b139b5ea714114722db08999cd6
salvatorebriga <salvo.z2302@gmail.com>
2024-07-22 16:09:32 +0200

    Added resource controller

---
+++ app/Http/Controllers/Admin/CocktailController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cocktail;
+use App\Http\Requests\StoreCocktailRequest;
+use App\Http\Requests\UpdateCocktailRequest;
+
+class CocktailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cocktails = Cocktail::all();
+
+        return view('cocktail', compact('cocktails'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCocktailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cocktail $cocktail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cocktail $cocktail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCocktailRequest $request, Cocktail $cocktail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cocktail $cocktail)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Guest\CocktailController;
+use App\Http\Controllers\Admin\CocktailController;
 use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/cocktail', [CocktailController::class, 'cocktail'])->name('cocktail');
+Route::get('/cocktails', [CocktailController::class, 'index'])->name('index');
