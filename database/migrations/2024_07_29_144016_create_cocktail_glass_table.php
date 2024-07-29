ef313ebdc505733a28f17481253a865dd80e4e64
salvatorebriga <salvo.z2302@gmail.com>
2024-07-29 16:56:33 +0200

    fix name

--- database/migrations/2024_07_29_144016_create_cocktail_glass_table.php
+++ database/migrations/2024_07_29_144016_create_cocktail_glass_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cocktails_glasses', function (Blueprint $table) {
+        Schema::create('cocktail_glass', function (Blueprint $table) {
             $table->id();
             $table->foreignId('cocktail_id')->constrained()->cascadeOnDelete();
             $table->foreignId('glass_id')->constrained()->cascadeOnDelete();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cocktails_glasses');
+        Schema::dropIfExists('cocktail_glass');
     }
 };
