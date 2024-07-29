4b5581480525fe2a83d2318aab7823f299418d99
salvatorebriga <salvo.z2302@gmail.com>
2024-07-29 16:22:42 +0200

    Added view and fix header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -10,7 +10,7 @@
             <a class="nav-link text-light fs-5" href="/cocktails">Cocktails</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light fs-5" href="/cocktails/create">Add New</a>
+            <a class="nav-link text-light fs-5" href="/cocktails/ingredients">Ingredients</a>
           </li>
         </ul>
         <span class="navbar-text text-light fs-5">
