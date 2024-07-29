b93c982ff3686a6f88fb607e24a0f50d95ee4a66
byHYONS <byhyons@gmail.com>
2024-07-29 16:11:04 +0200

    feat: spostato resource controller Ingredient in admin

--- app/Http/Controllers/Admin/IngredientController.php
+++ app/Http/Controllers/Admin/IngredientController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreIngredientRequest;
 use App\Http\Requests\UpdateIngredientRequest;
 use App\Models\Ingredient;
